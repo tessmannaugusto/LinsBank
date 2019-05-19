994f128b045538caf1787437b342c62b5c6d8517
Augusto Yunes <atessmannyunes@gmail.com>
2019-05-19 01:30:58 -0300

    Refactor Menu view from main

--- src/main/java/com/bank/Application/Program.java
+++ src/main/java/com/bank/Application/Program.java
@@ -2,7 +2,7 @@ package main.java.com.bank.Application;
 
 import main.java.com.bank.Entities.Account;
 import main.java.com.bank.Services.AccountServices;
-import main.java.com.bank.View.BalanceView;
+import main.java.com.bank.View.MenuView;
 
 import java.util.InputMismatchException;
 import java.util.Scanner;
@@ -14,6 +14,7 @@ public class Program {
         Scanner scanner = new Scanner(System.in);
         Account acc = new Account(123456, 1111, 0.0);
         AccountServices as = new AccountServices(acc);
+        MenuView menuView = new MenuView(acc);
         try {
             while (!as.isLogged()) {
                 System.out.println("Welcome to Lins Bank. Log in to your account to access bank services. ");
@@ -34,32 +35,8 @@ public class Program {
                 as.validateAccount(number, branch);
             }
 
-                int o = 0;
-                while (o != -1) {
-                    System.out.println();
-                    System.out.println("------------------------------");
-                    System.out.println();
-                    System.out.println("Menu: ");
-                    System.out.println("1. Deposit");
-                    System.out.println("2. Withdraw");
-                    System.out.println("3. Balance");
-                    System.out.print("Option (enter -1 to exit): ");
-                    o = scanner.nextInt();
-                    if (o == 1) {
-                        System.out.print("How much would you like to deposit? ");
-                        Double amount = scanner.nextDouble();
-                        as.deposit(amount);
-                    } else if (o == 2) {
-                        System.out.print("How much would you like to withdraw? ");
-                        Double withdraw = scanner.nextDouble();
-                        as.withdraw(withdraw);
-                    }else if( o == 3) {
-                        new BalanceView(acc).print();
-                    } else if (o != -1) {
-                        System.out.println("Wrong number, please choose again.");
-                    }
-                }
-                System.out.println("Program finished");
+            menuView.print();
+
 
         }catch (InputMismatchException e){
             System.out.println("Error: Account number, Account Branch, Deposit and withdraw inputs must contain only numbers." +
---
+++ src/main/java/com/bank/View/MenuView.java
@@ -0,0 +1,48 @@
+package main.java.com.bank.View;
+
+
+import main.java.com.bank.Entities.Account;
+import main.java.com.bank.Services.AccountServices;
+
+import java.util.Scanner;
+
+public class MenuView {
+    Scanner scanner = new Scanner(System.in);
+    private Account account;
+    private AccountServices accountServices;
+
+    public MenuView(Account account) {
+        this.account = account;
+        accountServices = new AccountServices(account);
+    }
+
+    public void print(){
+        int o = 0;
+        while (o != -1) {
+            System.out.println();
+            System.out.println("------------------------------");
+            System.out.println();
+            System.out.println("Menu: ");
+            System.out.println("1. Deposit");
+            System.out.println("2. Withdraw");
+            System.out.println("3. Balance");
+            System.out.print("Option (enter -1 to exit): ");
+            o = scanner.nextInt();
+            if (o == 1) {
+                System.out.print("How much would you like to deposit? ");
+                Double amount = scanner.nextDouble();
+                accountServices.deposit(amount);
+            } else if (o == 2) {
+                System.out.print("How much would you like to withdraw? ");
+                Double withdraw = scanner.nextDouble();
+                accountServices.withdraw(withdraw);
+            }else if( o == 3) {
+                new BalanceView(account).print();
+            } else if (o != -1) {
+                System.out.println("Wrong number, please choose again.");
+            }
+        }
+        System.out.println("Program finished");
+
+    }
+}
